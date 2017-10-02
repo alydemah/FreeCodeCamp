81fa3fad1c908c20779a86074bda1d452ac2091f
yladem <mohamedattaly@outlook.com>
2017-10-02 12:58:53 +0200

    Doing more basic javascript ex

---
+++ Basic-JavaScript/Build JavaScript Objects.js
@@ -0,0 +1,22 @@
+
+// Example
+var ourDog = {
+    "name": "Camper",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["everything!"]
+  };
+  
+  // Only change code below this line.
+  
+  var myDog = {
+    "name": "Dog",
+    "legs": 3,
+    "tails": 0,
+    "friends": ["Doby", "Ela", "Fan"]
+   
+    
+  
+    
+  };
+  
