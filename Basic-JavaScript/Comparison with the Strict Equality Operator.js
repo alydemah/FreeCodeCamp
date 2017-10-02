f913d4200103d2111f4d8fe9b2076bb5ce4a6c9d
yladem <mohamedattaly@outlook.com>
2017-10-02 11:40:16 +0200

    Adding more basic javascript ex

--- Basic-JavaScript/Comparison with the Strict Equality Operator.js
+++ Basic-JavaScript/Comparison with the Strict Equality Operator.js
@@ -1,12 +1,12 @@
 
 // Setup
-function testEqual(val) {
-    if (val == 12) { // Change this line
+function testStrict(val) {
+    if (val === 7) { // Change this line
       return "Equal";
     }
     return "Not Equal";
   }
   
   // Change this value to test
-  testEqual(10);
+  testStrict(10);
   
