c271ea985e93fffad446cfcfa3e01131f55595e5
yladem <mohamedattaly@outlook.com>
2017-10-11 12:17:47 +0200

    adding Basic js algorithim

---
+++ Basic-Algorithm-Scripting/Reverse a String.js
@@ -0,0 +1,12 @@
+
+function reverseString(str) {
+  var reversedStr = str.split("");
+  
+  reversedStr = reversedStr.reverse();
+  
+  reversedStr = reversedStr.join([separator = ""]);
+  
+  return reversedStr;
+}
+
+reverseString("hello");
